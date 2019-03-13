fc1f9daac23c0d8fc1f3fbb5247ba45df1f217b8
anmaxwell <aniamaxwell@gmail.com>
2019-03-13 22:46:18 +0000

    Add files via upload

---
+++ readingCSV.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  7 11:00:11 2019
+
+@author: account1
+"""
+
+#%%
+
+# Load CSV (using python)
+import csv
+import numpy
+filename = 'pima-indians-diabetes.data.csv'
+raw_data = open(filename, 'rt')
+reader = csv.reader(raw_data, delimiter=',', quoting=csv.QUOTE_NONE)
+x = list(reader)
+data = numpy.array(x).astype('float')
+print(data.shape)
+
+
+#%%
+
+# Load CSV
+import numpy
+filename = 'pima-indians-diabetes.data.csv'
+raw_data = open(filename, 'rt')
+data = numpy.loadtxt(raw_data, delimiter=",")
+print(data.shape)
+
+
+#%%
+
+# Load CSV from URL using NumPy
+from numpy import loadtxt
+from urllib.request import urlopen
+url = 'https://raw.githubusercontent.com/jbrownlee/Datasets/master/pima-indians-diabetes.data.csv'
+raw_data = urlopen(url)
+dataset = loadtxt(raw_data, delimiter=",")
+print(dataset.shape)
+
+
+#%%
+
+# Load CSV using Pandas
+import pandas
+filename = 'pima-indians-diabetes.data.csv'
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+data = pandas.read_csv(filename, names=names)
+print(data.shape)
+
+
+#%%
+
+# Load CSV using Pandas from URL
+import pandas
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/pima-indians-diabetes.data.csv"
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+data = pandas.read_csv(url, names=names)
+print(data.shape)
